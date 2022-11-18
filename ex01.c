32c0bfc50340df40960f934415fcbf72adb2aed6
RahLacerda <raisa.lcm303@gmail.com>
2022-11-18 14:51:28 -0300

    primeiro commit

---
+++ ex01.c
@@ -0,0 +1,17 @@
+//
+#include <stdio.h>
+int main()
+{
+    // declaração de variáveis
+    int idade;
+    float salario, valor;
+    // atribuição de variáveis
+    idade = 25;
+    valor = 4500.50;
+    salario = valor;
+    // saída dos dados
+    printf("Idade: %d\n", idade);
+    printf("E o salário: R$ %f \n", salario);
+    printf("Hello World!");
+    return 0;
+}
